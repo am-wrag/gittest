b5f285499b5cd420caeb048aa88b84644a87d794
Михаил Ковалев <wraggg@ya.ru>
2021-05-07 13:00:32 +0300

    sad

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //asd 
+            //IAsyncDisposable 
             Console.WriteLine("Hello World!");
         }
     }
